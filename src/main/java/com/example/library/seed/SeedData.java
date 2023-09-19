243859606fd61bb55595996b575650ee656b5b57
Ricardo Maya <rickmaya@gmail.com>
2023-09-19 10:00:10 -0500

    instantiate a user object in the SeedData

--- src/main/java/com/example/library/seed/SeedData.java
+++ src/main/java/com/example/library/seed/SeedData.java
@@ -1,5 +1,6 @@
 package com.example.library.seed;
 
+import com.example.library.model.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +23,15 @@ public class SeedData implements CommandLineRunner {
         this.passwordEncoder = passwordEncoder;
         this.bookRepository = bookRepository;
         this.genreRepository = genreRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setUserName("SlickRick");
+        user.setEmailAddress("rickmaya@gmail.com");
+        user.setPassword(passwordEncoder.encode("rick12345"));
+        userRepository.save(user);
     }
 
 }
