0c4bb7240b503a8b9a28ac7c9605aea2101d8a22
Ricardo Maya <rickmaya@gmail.com>
2023-09-19 09:45:44 -0500

    create UserRepository interface

---
+++ src/main/java/com/example/library/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.library.repository;
+
+import com.example.library.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmailAddress(String userEmailAddress);
+
+    User findUserByEmailAddress(String emailAddress);
+}
