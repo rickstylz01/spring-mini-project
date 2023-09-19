00c850f8f4e3f44583e814df1c536a02b489b8c4
Ricardo Maya <rickmaya@gmail.com>
2023-09-19 15:26:25 -0500

    create loginUser method to authenticate user based on login request

--- src/main/java/com/example/library/service/UserService.java
+++ src/main/java/com/example/library/service/UserService.java
@@ -2,11 +2,18 @@ package com.example.library.service;
 
 import com.example.library.exception.InformationExistException;
 import com.example.library.model.User;
+import com.example.library.model.request.LoginRequest;
 import com.example.library.repository.UserRepository;
+import com.example.library.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -43,5 +50,22 @@ public class UserService {
      */
     public User findUserByEmailAddress(String emailAddress) {
         return userRepository.findUserByEmailAddress(emailAddress);
+    }
+
+    /**
+     * Authenticate a user based on the provided login request and generates a JWT upon successful authentication
+     * @param loginRequest The login request containing user credentials
+     * @return An {@link Optional} containing the JWT token if authentication is successful, or empty if authentication fails
+     */
+    public Optional<String> loginUser(LoginRequest loginRequest) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(), loginRequest.getPassword());
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 }
