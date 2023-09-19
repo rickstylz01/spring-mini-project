3d21c2cb873b3481c31b787c1ec5a25204966bf2
Ricardo Maya <rickmaya@gmail.com>
2023-09-18 20:23:43 -0500

    update the orm for the Genre class

--- src/main/java/com/example/library/model/Genre.java
+++ src/main/java/com/example/library/model/Genre.java
@@ -1,9 +1,15 @@
 package com.example.library.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
@@ -27,4 +33,7 @@ public class Genre {
 
     @Column
     private int booksCount; // number of books within this genre
+
+    @ManyToMany(mappedBy = "genres")
+    private Set<Book> books = new HashSet<>();
 }
