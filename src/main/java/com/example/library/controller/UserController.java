79b66dae2b756619ca0b5f3da98e89a447604624
Ricardo Maya <rickmaya@gmail.com>
2023-09-19 12:49:12 -0500

    set dev profile as active in application properties

--- src/main/java/com/example/library/controller/UserController.java
+++ src/main/java/com/example/library/controller/UserController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/auth/users")
+@RequestMapping(path = "/auth/users") // http://localhost:9096/auth/users/
 public class UserController {
     private UserService userService;
 
     public void setUserService(UserService userService) { this.userService = userService; }
 
-    @PostMapping(path = "/register/")
+    @PostMapping(path = "/register/") // http://localhost:9096/auth/users/register/
     public User createUser(@RequestBody User userObject) {
         return userService.createUser(userObject);
     }
